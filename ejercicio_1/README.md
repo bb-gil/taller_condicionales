f4164b56736685ab74dc223fefbb577c55488d0f
bb-gil <alexisgil@colegioguanenta.edu.co>
2024-02-29 17:40:20 -0500

    se agrego el codigo python del programa

---
+++ ejercicio_1/README.md
@@ -0,0 +1,24 @@
+# EJERCICIO NO.1
+
+## programa que lea las coordenadas cartesianas (x, y) de un punto en el plano y calcule el cuadrante al cual pertenece el punto.
+
+# ANALISIS
+
+variables de entrada (input)
+
+x,y: coordenadas del punto
+variables de salida
+
+eje x
+eje y
+origen
+cuadrante 1
+cuadrante 2
+cuadrante 3
+cuadrante 4
+
+nos indicara el punto exacto en el plano cartesiano
+
+# DISEÑO
+
+![diagrama de flujo](diagrama.png "diagrama de flujo")
---
+++ ejercicio_1/cordenadas_cartecianas.py
@@ -0,0 +1,26 @@
+# Programa saber la ubicacion de x & y en el plano carteciano
+
+# input
+print("------------------------------------------------------")
+
+x = int(input("digite la posicion de x en el plano: "))
+y = int(input("digite la posicion de y en el plano: "))
+
+# procesing
+if x == 0:
+    if y == 0:
+        print(("la coordenada" ,(x , y), "esta en el origen"))
+    else:
+        print(("la coordenada" ,(x , y), "esta en el eje y"))
+elif y == 0:
+    print("estas en el eje y")
+elif x > 0:
+    if y > 0:
+        print("estas en el cuadrante 1")
+    else:
+        print("estas en el cuadrante 4")
+elif  y < 0:
+    if y < 0:
+        print("estas en el cuadrante 3")
+    else:
+        print("estas en el cuadrante 2")                                      
